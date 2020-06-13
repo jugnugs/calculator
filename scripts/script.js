8781aad3e74ca54e7866c82d382ea843a635d38d
Julia <julialn.sunni@gmail.com>
2020-06-13 11:42:15 -0400

    Created operation functions, implemented basic calculator interface

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+function operate(operator, a, b) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
